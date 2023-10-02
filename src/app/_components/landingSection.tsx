d4ad26568b5077ce3edc7c234134438aaa034007
Jesus Velarde <jev@umail.ucsb.edu>
2023-10-02 13:57:38 -0700

    separate landing section and make headshot responsive

---
+++ src/app/_components/landingSection.tsx
@@ -0,0 +1,71 @@
+'use client';
+import { FC, useEffect, useState } from 'react';
+import Image from 'next/image';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+export interface LandingSectionProps {};
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height
+    };
+}
+
+export const LandingSection: FC<LandingSectionProps> =  ({}) => {
+
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
+        <div className='text-2xl xl:text-5xl 2xl:text-5xl lg:text-4xl md:text-3xl font-bold text-white font-sans h-[100vh] p-10 md:p-24'>
+        <div className='flex flex-row content-center items-center'>
+          <div className='mr-20 md:mr-40'>
+            <p className='mb-5 text-sm md:text-xl font-light animate-fadeInFast'>
+              Hello, my name is
+            </p>
+            <p className='mb-5 text-[color:#9badcf] text-xl md:text-4xl lg:text-7xl animate-fadeIn'>
+              Jesus Velarde
+            </p>
+            <p className='animate-fadeInSlow text-base md:text-3xl lg:text-4xl xl:text-5xl'>
+              And I&apos;m a <span className='text-green'>Full-Stack Developer</span>
+            </p>
+
+          <div className='flex flex-row content-center items-center mt-10 animate-fadeInSlow text-grey'>
+            <a href='https://github.com/heyyysus' className='hover:text-white transition-color mr-5'>
+              <GitHubIcon sx={{
+                fontSize: 50,
+              }}  />
+            </a>
+            <a href='https://www.linkedin.com/in/jesus-velarde-arvayo-117958207/' className='hover:text-white transition-color pt-1.5'>
+              <LinkedInIcon sx={{
+                fontSize: 60,
+              }}  />
+            </a>
+          </div>
+
+          </div>
+          <div className='flex flex-col justify-center content-center animate-fadeInSlow'>
+            <div className='rounded-full overflow-hidden'>
+              <Image 
+                src={'/images/linkedin.jpeg'}
+                alt={''} 
+                width={windowDimensions.width < 768 ? 175 : 250}
+                height={windowDimensions.width < 768 ? 175 : 250}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,49 +3,12 @@ import SlotText from './_components/SlotText'
 import { AboutSection } from './_components/aboutSection'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { LandingSection } from './_components/landingSection';
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-space-between">
-      <div className='text-2xl xl:text-5xl 2xl:text-5xl lg:text-4xl md:text-3xl font-bold text-white font-sans h-[100vh] p-10 md:p-24'>
-        <div className='flex flex-row content-center items-center'>
-          <div className='mr-20 md:mr-40'>
-            <p className='mb-5 text-sm md:text-xl font-light animate-fadeInFast'>
-              Hello, my name is
-            </p>
-            <p className='mb-5 text-[color:#9badcf] text-xl md:text-4xl lg:text-7xl animate-fadeIn'>
-              Jesus Velarde
-            </p>
-            <p className='animate-fadeInSlow text-base md:text-3xl lg:text-4xl xl:text-5xl'>
-              And I&apos;m a <span className='text-green'>Full-Stack Developer</span>
-            </p>
-
-          <div className='flex flex-row content-center items-center mt-10 animate-fadeInSlow text-grey'>
-            <a href='https://github.com/heyyysus' className='hover:text-white transition-color mr-5'>
-              <GitHubIcon sx={{
-                fontSize: 50,
-              }}  />
-            </a>
-            <a href='https://www.linkedin.com/in/jesus-velarde-arvayo-117958207/' className='hover:text-white transition-color pt-1.5'>
-              <LinkedInIcon sx={{
-                fontSize: 60,
-              }}  />
-            </a>
-          </div>
-
-          </div>
-          <div className='flex flex-col justify-center content-center animate-fadeInSlow'>
-            <div className='rounded-full overflow-hidden'>
-              <Image 
-                src={'/images/linkedin.jpeg'}
-                alt={''} 
-                width={250}
-                height={250}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      <LandingSection />
       <AboutSection />
     </main>
   )
