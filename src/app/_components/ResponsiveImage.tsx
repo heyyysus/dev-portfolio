b30ad6e7c43ad51035ddb95f02619347385fec53
Jesus Velarde <jev@umail.ucsb.edu>
2023-10-06 13:34:17 -0700

    create better resposive image for landing section

---
+++ src/app/_components/ResponsiveImage.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import styles from './ResponsiveImage.module.css';
+
+export interface ResponsiveImageProps {
+    src: string;
+    alt: string;
+}; 
+
+export default function ResponsiveImage(props: ResponsiveImageProps) {
+  return (
+    <div className={styles.container}>
+      <Image 
+        src={props.src} 
+        alt={props.alt}
+        layout="fill"
+        objectFit="cover"
+      />
+    </div>
+  );
+}
